37117086aa4f99d20be3b91669b4c31d7e4ae68b
Bernard Chardonneau <bernard@pcbernard>
2018-06-11 21:58:00 +0200

    updating header comments

--- remnemo.c
+++ remnemo.c
@@ -1,5 +1,5 @@
 /*
- * Copyleft Bernard Chardonneau (2012) for initial version
+ * Copyleft Bernard Chardonneau (2012-+)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -19,8 +19,7 @@
  * Generic reformater for mnemonic interface files and man pages
  * after an Apertium translation.
  *
- * This source file permit to generate both apertium-remnemo and
- * apertium-reman executables.
+ * This source file permit to generate apertium-remnemo executable.
  *
  */
 
